442da225e8528ca0edb5cab44857c00ef8695acc
SamuelKelechi <kcfreshie@ymail.com>
2023-06-03 04:50:43 +0100

    Updated new component

--- src/Components/Home/Hero/Hero.js
+++ src/Components/Home/Hero/Hero.js
@@ -1,69 +1,19 @@
-import React,{useState} from 'react'
+import React from 'react'
 import './Hero.css'
-import Logo from '../../assets/logo.png'
-import {FaBars, FaTimes} from 'react-icons/fa'
+
 
 function Hero() {
-    const [toggle, setToggle] = useState(false);
-
-    const HandleToggle = () => {
-        setToggle(!toggle)
-    }
+ 
 
   return (
     <div className='Home-Hero-Continer'>
         <div className='Home-Hero-Wrapper'>
-            <div></div>
-
 
             <div className='Home-Hero-Center'>
                 <span>SABUR AKANBI OLUWA</span>
                 <h1>TYPINGS HERE...</h1>
             </div>
-
-            <div className='Header-Hold'>
-
-                <div className='Logo-Hold'>
-                    <img src={Logo} className='Logo' alt='Logo'/> SABUR AKANBI OLUWA
-                </div>
-
-
-                <div className='Navs-Hold'>
-                    <a href="#" class="link">
-                        <span class="text-out">Link</span>
-                        <span class="text-in" >Lets Get it</span>
-                    </a>
-                    <a href="#" class="link">
-                        <span class="text-out">Link</span>
-                        <span class="text-in" >Lets Get it</span>
-                    </a>
-                    <a href="#" class="link">
-                        <span class="text-out">Link</span>
-                        <span class="text-in" >Lets Get it</span>
-                    </a>
-                    <a href="#" class="link">
-                        <span class="text-out">Link</span>
-                        <span class="text-in" >Lets Get it</span>
-                    </a>
-                    <a href="#" class="link">
-                        <span class="text-out">Link</span>
-                        <span class="text-in" >Lets Get it</span>
-                    </a>
-                </div>
-
-                <div class="Mobile-Icons" onClick={HandleToggle} >
-                    {toggle ? (<FaTimes size={25} /> ):(<FaBars size={25} />)}
-                </div>
-
-            </div>
-            
-            {toggle && (
-                    <div className="drop-up-menu">
-                        <li>hi</li>
-                        <li>hi</li>
-                        <li>hi</li>
-                    </div>
-                )}
+           
         </div>
     </div>
   )
